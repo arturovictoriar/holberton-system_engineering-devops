d75d1334c15307ef4b91b854c099448414e4ca2d
arvicrin <1231@holbertonschool.com>
2020-04-04 00:59:58 +0000

    Add a custom HTTP header with Puppet, fix puppet-lint

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -15,18 +15,18 @@ file { '/var/www/html/index.html':
 }
 
 file_line { 'error':
-  ensure   => present,
-  path     => '/etc/nginx/sites-available/default',
-  after    => 'server_name _;',
-  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'server_name _;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
   require => Package['nginx'],
 }
 
 file_line { 'headercustom':
-  ensure   => present,
-  path     => '/etc/nginx/sites-available/default',
-  after    => ':80 default_server;',
-  line     => "add_header X-Served-By ${hostname};",
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  after   => ':80 default_server;',
+  line    => "add_header X-Served-By ${hostname};",
   require => Package['nginx'],
 }
 
