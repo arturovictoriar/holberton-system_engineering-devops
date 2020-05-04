63e5e3d3073b8b04d0eb815ba02d510b1467b3a7
arvicrin <1231@holbertonschool.com>
2020-05-04 09:31:17 +0000

    Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    Uses the fake API to get an employer and export the info in json formater
+"""
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    id_em = argv[1]
+    url_employ = "https://jsonplaceholder.typicode.com/users/{}".format(id_em)
+    url_todos = url_employ + "/todos"
+    r_employ = requests.get(url_employ).json()
+    r_todos = requests.get(url_todos).json()
+    username = r_employ.get("username")
+    total_num_task = r_todos
+    list_dict_report = []
+    for task in total_num_task:
+        id_report = {}
+        id_report["username"] = str(username)
+        id_report["completed"] = task["completed"]
+        id_report["task"] = str(task["title"])
+        list_dict_report.append(id_report)
+    report = {}
+    report[id_em] = list_dict_report
+    with open("USER_ID.json", "w") as fjson:
+        fjson.write(json.dumps(report))
