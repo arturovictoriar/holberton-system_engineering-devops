46fa4a49b5ad6564a827d4d3057e5b18be3602d8
arvicrin <1231@holbertonschool.com>
2020-05-04 09:30:59 +0000

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Uses the fake API to get an employer and export the info in csv formater
+"""
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    id_em = argv[1]
+    url_employ = "https://jsonplaceholder.typicode.com/users/{}".format(id_em)
+    url_todos = url_employ + "/todos"
+    r_employ = requests.get(url_employ).json()
+    r_todos = requests.get(url_todos).json()
+    username = r_employ.get("username")
+    total_num_task = r_todos
+    list_report = []
+    for task in total_num_task:
+        report = {}
+        report["USER_ID"] = str(task["userId"])
+        report["USERNAME"] = str(username)
+        report["TASK_COMPLETED_STATUS"] = str(task["completed"])
+        report["TASK_TITLE"] = str(task["title"])
+        list_report.append(report)
+    header = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+    with open("USER_ID.csv", "w") as fcsv:
+        f_csv = csv.DictWriter(fcsv, fieldnames=header, quoting=csv.QUOTE_ALL)
+        """f_csv.writeheader()"""
+        f_csv.writerows(list_report)
