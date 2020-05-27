fea17f51c2d0eab3f08aa40dfaa20184e2b2241d
arvicrin <1231@holbertonschool.com>
2020-05-27 19:02:51 +0000

    Debuggin nginx masive request

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Fix ulimit request
+exec { 'ulimit':
+  command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 2000\"/g" /etc/default/nginx',
+  provider => shell,
+}
+
+exec { 'restorenginx':
+  command  => 'service nginx restart',
+  provider => shell,
+  require => Exec['ulimit'],
+}
